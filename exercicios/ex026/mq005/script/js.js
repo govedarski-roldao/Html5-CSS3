8398cb7529e3d067f9fd20130d4f2d4a87afa23e
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2022-09-26 23:18:28 +0100

    Media

---
+++ exercicios/ex026/mq005/script/js.js
@@ -0,0 +1,20 @@
+function mudouTamanho(){
+    if(window.innerWidth >= 768) {
+        itens.style.display = "block"
+    } else {
+        itens.style.display = "none"
+    }
+}
+
+function clickMenu(){
+    if (itens.style.display == "block"){
+        itens.style.display = "none"
+    } else {
+        itens.style.display = "block"
+    }
+}
+   /* itens.style.display = "inline"
+}
+function nonclick(){
+    itens.style.display = "none"
+}*/
